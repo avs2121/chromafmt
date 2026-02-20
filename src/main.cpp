d3979ad32b434d498f06e2b03f49a4d882c17f1d
Aksel Schiøtz <schiotzaksel@gmail.com>
2026-02-20 14:38:35 +0100

    Mini language for hex-values added

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,78 @@
-#include <algorithm>
+
+#include <cstdint>
 #include <format>
-#include <iomanip>
 #include <iostream>
-#include <sstream>
-#include <string_view>
 
-struct QuotableString : std::string_view {};
+/*
+Primary template looks like template<Class T, class CharT> -> struct formatter.
+Here T is the type we are formatting, and charT is the character type of the string.
+  - Because the character type can be both char and wchar -> important distinction.
 
-template <> struct std::formatter<QuotableString, char> {
-  bool quoted = false;
+So basically says std::formatter<Color, char> -> The formatter formats Color when
+the format string is made out of char
 
-  template <class ParseContext>
-  constexpr ParseContext::iterator parse(ParseContext &ctx) {
-    auto it = ctx.begin();
-    if (it == ctx.end())
-      return it;
+The Template <> signals full specilization of std::formatter for the combination of <T =
+Color, CharT = char>
+*/
 
-    if (*it == '#') {
-      quoted = true;
-      ++it;
-    }
-    if (it != ctx.end() && *it != '}')
-      throw std::format_error("Invalid format args for QuotableString.");
-
-    return it;
-  }
-
-  template <class FmtContext>
-  FmtContext::iterator format(QuotableString s, FmtContext &ctx) const {
-    std::ostringstream out;
-    if (quoted)
-      out << std::quoted(s);
-    else
-      out << s;
-
-    return std::ranges::copy(std::move(out).str(), ctx.out()).out;
-  }
+struct Color
+{
+    uint8_t r{0};
+    uint8_t g{0};
+    uint8_t b{0};
 };
 
-int main() {
-  QuotableString a("be"), a2(R"( " be " )");
-  QuotableString b("a question");
-  std::cout << std::format("To {0} or not to {0}, that is {1}.\n", a, b);
-  std::cout << std::format("To {0:} or not to {0:}, that is {1:}.\n", a, b);
-  std::cout << std::format("To {0:#} or not to {0:#}, that is {1:#}.\n", a2, b);
+template <>
+struct std::formatter<Color>
+{
+    bool isHex = false;
+    constexpr auto parse(std::format_parse_context& ctx)  // sets internal state
+    {
+        auto it = ctx.begin();
+        if (it == ctx.end())
+            return it;
+
+        if (*it == 'h' || *it == 'H')
+        {
+            isHex = true;
+            ++it;
+        }
+        if (it != ctx.end() && *it != '}')
+            throw std::format_error("Invalid format args for QuotableString.");
+
+        return it;
+    }
+
+    auto format(Color c, std::format_context& ctx) const  // read the interal state
+    {
+        if (isHex)
+        {
+            uint32_t val = (c.r << 16 | c.g << 8 | c.b);
+            return std::format_to(ctx.out(), "#{:x}", val);
+        }
+        else
+            return std::format_to(ctx.out(), "({}, {}, {})", c.r, c.g, c.b);
+    }
+};
+
+int main()
+{
+    Color c1(200, 200, 100);
+    std::cout << std::format("col {}\n", c1);
+    std::cout << std::format("col {:h}\n", c1);
+    std::cout << std::format("col {:H}\n", c1);
 }
+
+/*
+Input to handle :
+std::format("col {}\n", Color{100, 255, 255}); -> col(100, 255, 255)
+std::format("col {:h}\n", Color{100, 255, 255}); -> col #64c8ff
+
+{:*>10} -> fills the width to the left with '*' if empty -> *****hello
+{:*<10} -> fills the width to the right with '*' if empty ->  hello*****
+{:*^10} -> fills the width to right and left with '*' if empty -> **hello***
+
+format specification -> {'[arg-id]':'[format-spec]'}
+  -> {0:*>10} -> first argument, fills width with '*' to the left of the argument
+
+*/
